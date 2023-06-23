9aa50afe0dccde0fbdf12d010523b104d985d448
Isaieva Mariia <maria.m.isaieva@icloud.com>
2023-06-23 10:43:22 +0200

    API, E2E, Regressions tests

--- src/main/java/pages/StudentProfilePage.java
+++ src/main/java/pages/StudentProfilePage.java
@@ -7,20 +7,11 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentProfilePage {
-    public HeaderMenuUser headerMenuStudent = new HeaderMenuUser();
+    public HeaderMenuUser headerMenuUser = new HeaderMenuUser();
     private static SelenideElement studentName = $x("//h1");
     private static SelenideElement studentEmail = $x("//p");
-    //private static SelenideElement studentRole = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[1]/div[2]/div[3]/div/div/div/span/span");
     private static SelenideElement studentRole = $x("//span[@style='font-size: 1rem; color: rgb(249, 246, 238);']");
-    // private static SelenideElement studentPhoto = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[1]/div[1]/div/div/div/div");
     private static SelenideElement studentPhoto = $x("//div[@class='static-image']");
-//    private static SelenideElement studentAboutMe = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[2]/div/div/div/div/div/p");
-//    private static SelenideElement majorField = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[3]/div[1]/p");
-//    private static SelenideElement majorName = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[3]/div[1]/div/div/div/div/p");
-//    private static SelenideElement BackToDirectoryBtn = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[3]/div[2]/div/div/a");
-//    private static SelenideElement studentRoleField = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[3]/div[3]/p");
-//    private static SelenideElement studentRoleName = $x("/html/body/div[1]/div[2]/section/div/div/div/div/div/div[3]/div[3]/div/div/div/div/div");
-
     private static SelenideElement studentAboutMe = $x("//div[@class='sw-font-size-l sw-text-color-5A5D63 sw-font-family-default sw-font-weight-normal sw-text-align-left sw-letter-spacing-normal sw-padding-top-5xs sw-padding-bottom-7xs sw-padding-left-none sw-padding-right-none sw-margin-top-none sw-margin-bottom-none rich-text MuiBox-root css-dne634']");
     private static SelenideElement majorField = $x("//p[@class='sw-font-size-m sw-text-color-2c2921 sw-font-family-default sw-font-weight-extrabold sw-text-align-left sw-letter-spacing-normal sw-padding-top-6xs sw-padding-bottom-none sw-padding-left-none sw-padding-right-none MuiBox-root css-0']");
     private static SelenideElement majorName = $x("//p[@class='sw-font-size-l sw-text-color-967110 sw-font-family-default sw-font-weight-normal sw-text-align-left sw-letter-spacing-normal sw-padding-top-none sw-padding-bottom-5xs sw-padding-left-none sw-padding-right-none MuiBox-root css-1oazf1f']");
